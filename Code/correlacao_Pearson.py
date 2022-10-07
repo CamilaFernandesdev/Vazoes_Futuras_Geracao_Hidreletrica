e4afd7fea43e05782c3012326c692840d9b83b98
CAMILA FERNANDES <camila.lopes@copel.com>
2022-10-07 19:52:09 -0300

    Quase terminando

--- Code/correlacao_Pearson.py
+++ Code/correlacao_Pearson.py
@@ -4,6 +4,10 @@
 Created on Thu Sep 29 14:56:21 2022.
 
 @author: E805511
+
+ESTATÍSTICA DESCRITIVA
+
+
 """
 
 import pandas as pd
@@ -12,34 +16,16 @@ from pathlib import Path
 from copy import deepcopy
 from collections import deque
 
-#%%
-"""
-ESTATÍSTICA DESCRITIVA
-
-CORRELAÇÃO DE PEARSON:
-    
-    Quando uma hipótese de aumento ou queda de uma variável está associado à
-    evolução de outra variável, aplica-se o coeficiente de correlação de Pearson
-    com o intervalo de -1 a 1. O valor 0 indica que não há correlação entre as 
-    duas variáveis
-    
-    O coeficiente de correlação de Pearson mede o associação linear entre variáveis. Seu valor pode ser interpretado da seguinte forma:
-
-        +1 - Correlação positiva completa
-        +0,8 - Correlação positiva forte
-        +0,6 - Correlação positiva moderada
-        0 - nenhuma correlação
-        -0,6 - Correlação negativa moderada
-        -0,8 - Correlação negativa forte
-        -1 - Correlação negativa completa
-"""
 
 
-#meses = 'JAN FEV MAR ABR MAI JUN JUL AGO SET OUT NOV DEZ'.split()
-MESES = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+MESES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+USINAS_PRINCIPAIS = ('FURNAS', 'GBM', 'TUCURUÍ', 'SOBRADINHO')
 
-def leitura_vazao(end) -> pd.DataFrame:
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        
+def leitura_vazao(end: Path) -> pd.DataFrame:
     """Leitura do arquivo vazões.dat."""
     tab = pd.read_csv(end, sep='\s+', header = None )
     cab = ['POSTO', 'ANOS'] + MESES
@@ -47,146 +33,213 @@ def leitura_vazao(end) -> pd.DataFrame:
     tab.set_index(['POSTO'], drop=True, inplace=True)
     return tab
     
-caminho = Path(r'C:\Users\E805511\Downloads\vazoes 2022')
-df_vazao_original = leitura_vazao(caminho)
 
-#------------------------------------------------------------------------------
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+# %% GERANDO TABELA AUXILIAR
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-# %% 
-
-
-def organizando_dados():
-    """Digitar depois..."""
+def reordenando_dados(mes: int) -> pd.DataFrame :
+    """Versionamento dos dados.
+    
+    mes = mês que finaliza a previsão da Refinitiv
+    
+    Seleciona em qual mês inicia, realocando os meses anteriores.
+    No caso, inicia em Abril...
+    """
     #--------------------------------------------------------------
-    selecao1 = ~(df_vazao_original.loc[:, 'ANOS'] == 2022).values
+    #Seleciona o primeiro e o último ano
+    #Retorno em bool. '~ inverte True em False e vice-versa
+    selecao1 = ~(df_vazao_original.loc[:, 'ANOS'] == 2023).values
     selecao2 = ~(df_vazao_original.loc[:, 'ANOS'] == 1931).values
     #--------------------------------------------------------------
+    #Separando o arquivo em dois
     df_end_1 = deepcopy(df_vazao_original[selecao1])
     df_end_2 = deepcopy(df_vazao_original[selecao2])
     #--------------------------------------------------------------
-    df_end_1.rename(columns={'ANOS': 'ANO_INI'}, inplace=True)
+    #Renomeação da coluna
     df_end_2.rename(columns={'ANOS': 'ANO_FIM'}, inplace=True)
     #--------------------------------------------------------------
-    #Seleciona em qual mês inicia, realocando os meses anteriores
-    #No caso, inicia em Abril
-    df_end_1.loc[:, 1:3] = df_end_2.loc[:, 1:3]
+    #Seleciona em qual mês inicia, realocando os meses
+    #Une os DataFrames
+    df_end_1.loc[:, 1:mes] = df_end_2.loc[:, 1:mes]
     
     return df_end_1
 
-def organizando_colunas():
+def reordenando_cabecalho_meses(mes: int) -> list:
     """Para modificação do nome das colunas relacionada aos meses.
+    
     Seleção da como começam a tabela atráves da biblioteca collections,
     classe deque.
     -------------------
     Exemplo:
     com o rotate=0
-    imprime: deque(['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN',
-                    'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'])
-    com rotate= -3
+    imprime: deque([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+    
+    Com rotate = -3
     imprime: deque([4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3])
     equivalente: deque(['ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET',
                         'OUT', 'NOV', 'DEZ', 'JAN', 'FEV', 'MAR'])
     ------------------
     """
-    anos_sel = deque(MESES)
-    anos_sel.rotate(-3)
-    print(anos_sel)
-    return anos_sel
+    meses_reordenandos = deque(MESES)
+    meses_reordenandos.rotate(- mes)
+    print(meses_reordenandos)
+    return meses_reordenandos
 
-
-
-def tabela_auxiliar(): 
+def tabela_auxiliar(mes:int) -> pd.DataFrame : 
     """Digitar depois."""
-    #------------------------------------------------------------------------------
+    mes = mes_previsao
+    #--------------------------------------------------------------------------
     #Tabela organizada
-    anos_sel = organizando_colunas()
-    dados = organizando_dados()
-    #------------------------------------------------------------------------------
-    df_final = dados.loc[:, ['ANO_INI'] + list(anos_sel)]
-    df_final['ANO_INI'] = df_final['ANO_INI'].astype(str) + '-' + (df_final['ANO_INI'] + 1).astype(str)
-    #------------------------------------------------------------------------------
-    return df_final
+    meses_reordenando = reordenando_cabecalho_meses(mes)
+    dados = reordenando_dados(mes)
+    #--------------------------------------------------------------------------
+    df_tabela_aux = dados.loc[:, ['ANOS'] + list(meses_reordenando)]
+    df_tabela_aux['ANOS'] = df_tabela_aux['ANOS'].astype(str) + '-' + (df_tabela_aux['ANOS'] + 1).astype(str)
+    #--------------------------------------------------------------------------
+    return df_tabela_aux
     
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+#%% SELEÇÃO DA USINA
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-tabela_aux = tabela_auxiliar()
-#%% SELECIONAR UMA OU MúlTIPLAS USINAS
-# Código das maiores usinas de cada submercado de energia
-"""Apenas uma usina por enquanto"""
 def selecione_usina(cod:int) -> pd.DataFrame:
-    """Digite o código da usina."""
-    teste_usinas = tabela_aux.groupby("POSTO").get_group(cod)
+    """Digite o código da usina.
+    
+    Código das maiores usinas de cada submercado de energia.
+    """
+    dados_usinas_selecionada = tabela_aux.groupby("POSTO").get_group(cod)
     if cod == 6:
-        print('FURNAS')
+        print(USINAS_PRINCIPAIS[0])
     elif cod == 74:
-        print('GBM')
+        print(USINAS_PRINCIPAIS[1])
     elif cod == 169:
-        print('TUCURUÍ')
+        print(USINAS_PRINCIPAIS[2])
     elif cod == 275:
-        print('SOBRADINHO')
+        print(USINAS_PRINCIPAIS[3])
     else:
         print('Escolha entre as usinas cod:[6, 74, 169, 275]')
     
-    return teste_usinas
+    return dados_usinas_selecionada
 
 
-usina_sel = selecione_usina(74)
-
-# =============================================================================
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 # %% CÁLCULO FABIANO
-# =============================================================================
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-def periodo_correlacao():
+def series_anos():
     """Escrever depois."""
     #-----------------------------------------------------------
     #Seleção da coluna com os anos da tabela auxiliar
-    series_anos = usina_sel['ANO_INI']
+    series_anos = usina_sel['ANOS']
     series_anos.reset_index(drop=True, inplace=True)
     #-----------------------------------------------------------
-    usina_sel.set_index('ANO_INI', inplace=True)
+    usina_sel.set_index('ANOS', inplace=True)
     #-----------------------------------------------------------
-    
     return series_anos
 
-series_anos = periodo_correlacao()
-
     
-def calculo_correlacao():
-    """Milhões de coisa em uma função.
-    imprime: período da correlação: 2018-2019
+def correlacao():
+    r"""Milhões de coisa em uma função.
+    
+    .. math:: R_{ij}=\\frac{ C_{ij} }{ \\sqrt{ C_{ii} * C_{jj}}}
+    
+    The values of `R` are between -1 and 1, inclusive.
     """
     #--------------------------------------------------------------------------
     #função é usada para acessar a última linha do dataframe
-    x = np.array(usina_sel.tail(1))
+    #x = np.array(usina_sel.tail(1))
+    x1 = np.array(usina_sel.iloc[-2, :])
     #--------------------------------------------------------------------------
     #Cálculo correlação
-    correlacao = np.corrcoef(x=x, y=usina_sel)[1: -1, 0]
+    correlacao = np.corrcoef(x=x1, y=usina_sel)[1: -2, 0]
     #--------------------------------------------------------------------------
-   
-    
     return correlacao
 
-
-resultado_correlacao = calculo_correlacao()
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 #%% Preenchimento
-
-# Preenchimento
-ano_preenchimento = series_anos[resultado_correlacao.argmax()+1]
-#--------------------------------------------------------------------------
-
-ano_escolhido = series_anos[resultado_correlacao.argmax()]
-#--------------------------------------------------------------------------
-
-print(f'período da correlação: {ano_escolhido}')
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 
-teste = tabela_aux.groupby("ANO_INI").get_group(ano_preenchimento)
+def periodo_da_correlacao():
+    """descrever."""
+    # Preenchimento
+    ano_preenchimento = series_anos[resultado_correlacao.argmax()+1]
+    #--------------------------------------------------------------------------
+    #
+    ano_escolhido = series_anos[resultado_correlacao.argmax()]
+    print(f'período da correlação: {ano_escolhido}')
+    return ano_preenchimento
 
 
+def não_sei():
+    """Imagine descrever o que eu não sei."""    
+    #--------------------------------------------------------------------------
+    teste = tabela_aux.groupby("ANOS").get_group(ano_preenchimento)
+    #--------------------------------------------------------------------------
+    # #Selecionando até Dezembro
+    # Até o memento as previsões 
+    teste = teste.loc[:, :12]
+    
+    return teste
 
 
-#%% Escrever no arquivo txt
+def inserindo_resuldado_correlacao():
+    """Cansada, depois escrevo."""
+    #--------------------------------------------------------------------------
+    #
+    criterio_selecao = df_vazao_original.loc[:, 'ANOS'] == 2023
+    #--------------------------------------------------------------------------
+    #
+    df_vazao_original.loc[criterio_selecao, 4:12] = teste
+    #--------------------------------------------------------------------------
+    #
+    df_final_previsoes = df_vazao_original
+    #--------------------------------------------------------------------------
+    return df_final_previsoes
 
-# with open(file = end, encoding='utf-8') as arquivo:
-#     vazoes_novo = arquivo.readline()
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+# %%EScrever para texto
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 
+def converter_to_txt():
+    """Retorna o arquivo com as alterações e acrescimos dos cenários de previsão."""
+    df_final_previsoes.to_csv(r'C:/Users/E805511/Downloads/vazoes_{USINAS_PRINCIPAIS}.txt',
+                             header= None, 
+                             index=True, 
+                             sep='\t',
+                             mode='a')
+
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+def main():
+    """Só sei que tenho que fazer assim."""
+    pass
+   
+
+caminho = Path('C:/Users/E805511/Downloads/vazoes.txt')
+cod = 6
+mes_previsao = 3
+
+
+df_vazao_original = leitura_vazao(caminho)
+    
+tabela_aux = tabela_auxiliar(mes=mes_previsao)
+
+usina_sel = selecione_usina(cod=cod)
+
+series_anos = series_anos()
+
+resultado_correlacao = correlacao()
+
+ano_preenchimento = periodo_da_correlacao()
+
+teste = não_sei()
+
+df_final_previsoes = inserindo_resuldado_correlacao()
+    
+    
+
+
+#mes_previsao = input(f'Digito do mês que termina a previsão da Refinitiv: ')
